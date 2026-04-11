98ccdba69d145a2d0a44e0bb600efd0790c9d9cf
Jayashree Pujari <jayashreepujari@Ravirajs-MacBook-Air.local>
2026-04-11 10:01:51 +0530

    Phase 2: Advanced features

---
+++ main_integrated.py
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error>
+  <Code>NoSuchBucket</Code>
+  <Message>The specified bucket does not exist.</Message>
+  <RequestId>69D9CEB735C35032305F250D</RequestId>
+  <HostId>kimi-files.oss-cn-beijing.aliyuncs.com</HostId>
+  <BucketName>kimi-files</BucketName>
+  <EC>0015-00000101</EC>
+  <RecommendDoc>https://api.aliyun.com/troubleshoot?q=0015-00000101</RecommendDoc>
+</Error>
